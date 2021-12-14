93de7cf35b4838ccd20f64b73bd6cfcc89b373a9
Chris Judkins <chrisjudk@protonmail.com>
2021-12-14 16:57:18 -0500

    Added database
    Features:
    -History table

---
+++ TimeFromSeconds/DBConnector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Controller
+{
+    public static class DBConnector
+    {
+        public static void Initialize()
+        {
+            using (SQLiteConnection conn = new SQLiteConnection("Data Source=TFS.db;Version=3;"))
+            {
+                using(SQLiteCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "" +
+                        "CREATE TABLE IF NOT EXISTS History (" +
+                        "Input REAL NOT NULL," +
+                        "Unit INTEGER NOT NULL," +
+                        "Time NUMERIC NOT NULL," +
+                        "PRIMARY KEY (Input,Time));";
+                    cmd.ExecuteNonQuery();
+                }
+            }//conn
+        }//Initialize()
+    }//DBConnector
+}//namespace
